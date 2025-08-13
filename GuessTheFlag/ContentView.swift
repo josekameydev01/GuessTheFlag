f100d2f261d0a5990e49e10375249f30e7240a6a
josekameydev01 <gk01cracklitos@gmail.com>
2025-08-12 20:04:14 -0600

    Small challenge completed

--- GuessTheFlag/ContentView.swift
+++ GuessTheFlag/ContentView.swift
@@ -9,12 +9,22 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Text("First View")
-                .background(Color.red)
-            Text("Second view")
-                .background(Color.blue)
-            Spacer()
+        VStack(spacing: 25) {
+            HStack(spacing: 25) {
+                Text("1")
+                Text("2")
+                Text("3")
+            }
+            HStack(spacing: 25) {
+                Text("4")
+                Text("5")
+                Text("6")
+            }
+            HStack(spacing: 25) {
+                Text("7")
+                Text("8")
+                Text("9")
+            }
         }
     }
 }
