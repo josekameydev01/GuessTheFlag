30e52bd6930125e6ab4cb523aedad1d467201328
josekameydev01 <gk01cracklitos@gmail.com>
2025-08-12 20:08:35 -0600

    How to use a ZStack

--- GuessTheFlag/ContentView.swift
+++ GuessTheFlag/ContentView.swift
@@ -9,22 +9,11 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack(spacing: 25) {
-            HStack(spacing: 25) {
-                Text("1")
-                Text("2")
-                Text("3")
-            }
-            HStack(spacing: 25) {
-                Text("4")
-                Text("5")
-                Text("6")
-            }
-            HStack(spacing: 25) {
-                Text("7")
-                Text("8")
-                Text("9")
-            }
+        ZStack(alignment: .center) {
+            Text("Hello, World!")
+                .font(.largeTitle)
+            Text("Hello")
+            
         }
     }
 }
