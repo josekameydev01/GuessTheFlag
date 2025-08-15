d4966c849915bb688d8a68b1bc831db9e128d6fb
josekameydev01 <gk01cracklitos@gmail.com>
2025-08-14 19:46:33 -0600

    How to use gradients

--- GuessTheFlag/ContentView.swift
+++ GuessTheFlag/ContentView.swift
@@ -9,18 +9,17 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        ZStack {
-            VStack(spacing: 0) {
-                Color.red
-                Color.blue
-            }
-            
-            Text("Your content")
-                .foregroundStyle(.secondary)
-                .padding(50)
-                .background(.ultraThinMaterial)
-        }
-        .ignoresSafeArea()
+//        LinearGradient(colors: [.black, .white], startPoint: .top, endPoint: .bottom)
+//        LinearGradient(stops: [
+//            Gradient.Stop(color: .white, location: 0.20),
+//            Gradient.Stop(color: .black, location: 1),
+//        ], startPoint: .top, endPoint: .bottom)
+//        RadialGradient(colors: [.black, .white], center: .center, startRadius: 20, endRadius: 200)
+//        AngularGradient(colors: [.red, .yellow, .green, .blue, .purple, .red], center: .center)
+        Text("Your content")
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+            .foregroundStyle(.white)
+            .background(.green.gradient)
     }
 }
 
