65bd6ada8f556d6e4003d88f2b45146313f8793b
josekameydev01 <gk01cracklitos@gmail.com>
2025-08-16 21:42:11 -0600

    How to show alert messages

--- GuessTheFlag/ContentView.swift
+++ GuessTheFlag/ContentView.swift
@@ -8,38 +8,17 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var showingAlert = false
     var body: some View {
-        Button("Press me", role: .destructive, action: printSomething)
-        Button("Button 1") {}
-            .buttonStyle(.bordered)
-            .tint(.purple)
-        Button("Button 2") {}
-            .buttonStyle(.borderless)
-        Button("Button 3") {}
-            .buttonStyle(.borderedProminent)
-            .tint(.red)
-        Button {
-            print("This button was tapped!")
-        } label: {
-            Text("Tap me")
-                .padding()
-                .foregroundStyle(.white)
-                .background(.red)
+        Button("Show Alert") {
+            showingAlert = true
         }
-        
-        Button {
-            print("Edit button was tapped!")
-        } label: {
-            Image(systemName: "pencil")
+        .alert("Important Message", isPresented: $showingAlert) {
+            Button("Cancel", role: .cancel) { }
+            Button("Delete", role: .destructive) { }
+        } message: {
+            Text("Please read this information it is very important.")
         }
-        
-        Button("Edit", systemImage: "pencil") {
-            print("The edit button was taped")
-        }
-    }
-    
-    func printSomething() {
-        print("The button was tapped!")
     }
 }
 
